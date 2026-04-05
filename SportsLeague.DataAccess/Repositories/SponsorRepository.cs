45dfd518dee952a155f1a705f5d798f110e04fdc
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-05 12:49:15 -0500

    ISponsorRepository interface implemented and SponsorRepository created

---
+++ SportsLeague.DataAccess/Repositories/SponsorRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+using SportsLeague.Domain.Interfaces.Repositories;
+
+namespace SportsLeague.DataAccess.Repositories;
+
+public class SponsorRepository : GenericRepository<Sponsor>, ISponsorRepository
+{
+    public SponsorRepository(LeagueDbContext context) : base(context)
+    {
+    }
+
+    public async Task<bool> ExistByNameAsync(string name)
+    {
+        return await _dbSet.AnyAsync(s => s.Name == name);
+    }
+
+    public async Task<bool> ExistByEmailAsync(string email)
+    {
+        return await _dbSet.AnyAsync(s => s.ContactEmail == email);
+    }
+
+    public async Task<IEnumerable<Sponsor>> GetByCategoryAsync(SponsorCategory category)
+    {
+        return await _dbSet
+            .Where(s => s.Category == category)
+            .ToListAsync();
+    }
+
+    public async Task<Sponsor?> GetByIdWithTournamentsAsync(int id)
+    {
+        return await _dbSet
+            .Where(s => s.Id == id)
+            .Include(s => s.TournamentSponsors)
+            .ThenInclude(ts => ts.Tournament)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<Sponsor?> GetByNameAsync(string name)
+    {
+        return await _dbSet
+            .Where(s => s.Name == name)
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task<IEnumerable<Sponsor>> GetByTournamentAsync(int tournamentId)
+    {
+        return await _context.TournamentSponsors
+            .Where(ts => ts.TournamentId == tournamentId)
+            .Select(ts => ts.Sponsor)
+            .ToListAsync();
+    }
+}
---
+++ SportsLeague.Domain/Interfaces/Repositories/ISponsorRepository.cs
@@ -0,0 +1,15 @@
+﻿using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Interfaces.Repositories; 
+
+public interface ISponsorRepository : IGenericRepository<Sponsor>
+{
+    Task<Sponsor?> GetByNameAsync(string name);
+    Task<IEnumerable<Sponsor>> GetByCategoryAsync(SponsorCategory category);
+    Task<IEnumerable<Sponsor>> GetByTournamentAsync(int tournamentId);
+    Task<Sponsor> GetByIdWithTournamentsAsync(int id);
+    Task<bool> ExistByNameAsync(string name);
+    Task<bool> ExistByEmailAsync(string email);
+
+}
