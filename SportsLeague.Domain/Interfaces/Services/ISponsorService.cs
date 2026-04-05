0e7f7a38e56a1bc8a8a1b81a0672b2198d35d659
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-05 15:40:42 -0500

    ISponsorService created

---
+++ SportsLeague.Domain/Interfaces/Services/ISponsorService.cs
@@ -0,0 +1,19 @@
+﻿using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Enums;
+
+namespace SportsLeague.Domain.Interfaces.Services;
+
+public interface ISponsorService
+{
+    Task<IEnumerable<Sponsor>> GetAllAsync();
+    Task<Sponsor?> GetByIdAsync(int id);
+    Task<Sponsor?> GetByIdWithTournamentAsync(int id);
+    Task<Sponsor?> GetByNameAsync(string name);
+    Task<Sponsor> CreateAsync(Sponsor sponsor);
+    Task UpdateAsync(int id, Sponsor sponsor);
+    Task DeleteAsync(int id);
+    Task UpdateCategoryAsync(int id, SponsorCategory newCategory);
+    Task RegisterSponsorToTournamentAsync(int tournamentId, int sponsorId, decimal contractAmount, DateTime? joinedAt = null);
+    Task UnregisterSponsorFromTournamentAsync(int tournamentId, int sponsorId);
+    Task<IEnumerable<Sponsor>> GetSponsorsByTournamentAsync(int tournamentId);
+}
