0a77492fe8bd7365a06b8ef2cd199d6688a43604
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-06 17:15:08 -0500

    TournamentSponsorDTO and SponsorWithSponsorshipsDTO created, the last one shows the sponsorship details.

---
+++ SportsLeague.API/DTOs/ResponseDTOs/SponsorWithSponsorshipsDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace SportsLeague.API.DTOs.ResponseDTOs;
+
+public class SponsorWithSponsorshipsDTO : SponsorResponseDTO
+{
+    public IEnumerable<TournamentSponsorDTO> Sponsorships { get; set; } = new List<TournamentSponsorDTO>();
+}
+
---
+++ SportsLeague.API/DTOs/ResponseDTOs/TournamentSponsorDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace SportsLeague.API.DTOs.ResponseDTOs;
+
+public class TournamentSponsorDTO
+{
+    public int TournamentId { get; set; }
+    public string TournamentName { get; set; } = string.Empty; // opcional para UI
+    public decimal ContractAmount { get; set; }
+    public DateTime JoinedAt { get; set; }
+}
+
