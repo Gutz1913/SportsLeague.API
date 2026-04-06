de0c0aeaa69e565194362989e31d3d94624da8fc
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-06 17:33:32 -0500

    Automaper successfully updated.

--- SportsLeague.API/Mappings/MappingProfile.cs
+++ SportsLeague.API/Mappings/MappingProfile.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using SportsLeague.API.DTOs.Request;
+using SportsLeague.API.DTOs.RequestDTOs;
 using SportsLeague.API.DTOs.Response;
+using SportsLeague.API.DTOs.ResponseDTOs;
 using SportsLeague.Domain.Entities;
 
 namespace SportsLeague.API.Mappings;
@@ -32,6 +34,30 @@ public class MappingProfile : Profile
                 opt => opt.MapFrom(src =>
                     src.TournamentTeams != null ? src.TournamentTeams.Count : 0));
 
+        //Sponsor mappings
+        CreateMap<SponsorRequestDTO, Sponsor>();
+        CreateMap<Sponsor, SponsorResponseDTO>();
+
+        //TournamentSponsor mappings 
+        CreateMap<TournamentSponsor, TournamentSponsorDTO>()
+            .ForMember(dest => dest.TournamentName, opt => opt.MapFrom(src => src.Tournament != null ? src.Tournament.Name : string.Empty));
+
+        // TournamentSponsor (la entidad de unión con payload) -> DTO
+        CreateMap<TournamentSponsor, TournamentSponsorDTO>()
+            .ForMember(dest => dest.TournamentName, opt => opt.MapFrom(src => src.Tournament != null ? src.Tournament.Name : string.Empty));
+
+        // Sponsor with sponsorships (composed response)
+        CreateMap<Sponsor, SponsorWithSponsorshipsDTO>()
+            .ForMember(dest => dest.Sponsorships, opt => opt.MapFrom(src => src.TournamentSponsors));
+
+        // RegisterSponsorDTO -> TournamentSponsor (uso en endpoints: necesitas setear TournamentId en el controlador o servicio)
+        CreateMap<RegisterSponsorDTO, TournamentSponsor>()
+            .ForMember(dest => dest.JoinedAt, opt => opt.MapFrom(src => src.JoinedAt ?? DateTime.UtcNow));
+
+        // Optional: Update category DTO -> Sponsor (if you accept it as payload)
+        CreateMap<UpdateSponsorCategoryDTO, Sponsor>()
+            .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.Category));
     }
 }
+
 
