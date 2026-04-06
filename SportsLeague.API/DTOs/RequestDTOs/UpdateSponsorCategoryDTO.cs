e8144b76754ed6592d43c17d28c7644e8c5d5c5c
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-06 16:49:27 -0500

    Created UpdateSponsorCategory DTO

---
+++ SportsLeague.API/DTOs/RequestDTOs/UpdateSponsorCategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using SportsLeague.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SportsLeague.API.DTOs.RequestDTOs;
+
+public class UpdateSponsorCategoryDTO
+{
+    [Required]
+    public SponsorCategory Category { get; set; }
+}
