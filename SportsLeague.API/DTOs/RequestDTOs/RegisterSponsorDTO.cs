65379a773c4b46cf60fb32af194ba2b8aee0cba9
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-06 16:40:24 -0500

    Created register sponsor DTO.

---
+++ SportsLeague.API/DTOs/RequestDTOs/RegisterSponsorDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace SportsLeague.API.DTOs.RequestDTOs;
+
+public class RegisterSponsorDTO
+{
+    public int SponsorId { get; set; }
+    public decimal ContractAmount { get; set; }
+    public DateTime? JoinedAt { get; set; } // opcional; si es null se usará UTC now en el servicio
+}
