01963ae569a32552f1df69424c7360a0b0bdf1bd
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-20 18:51:48 -0500

    Interfaces and services created.

---
+++ SportsLeague.Domain/Interfaces/Repositories/IMatchResultRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsLeague.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportsLeague.Domain.Interfaces.Repositories;
+
+public interface IMatchResultRepository : IGenericRepository<MatchResult>
+{
+    Task<MatchResult?> GetByMatchIdAsync(int matchId);
+}
