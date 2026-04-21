7a80bcff992ac1da3b6d18893fb33bbf67fb0007
Gutz1913 <andresfgutierrezv@gmail.com>
2026-04-20 19:01:05 -0500

    Repositories implemented.

---
+++ SportsLeague.DataAccess/Repositories/MatchResultRepository.cs
@@ -0,0 +1,15 @@
+﻿using SportsLeague.DataAccess.Context;
+using SportsLeague.Domain.Entities;
+using SportsLeague.Domain.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+namespace SportsLeague.DataAccess.Repositories;
+
+public class MatchResultRepository : GenericRepository<MatchResult>, IMatchResultRepository
+{
+    public MatchResultRepository(LeagueDbContext context) : base(context) { }
+
+    public async Task<MatchResult?> GetByMatchIdAsync(int matchId)
+    {
+        return await _dbSet.FirstOrDefaultAsync(mr => mr.MatchId == matchId);
+    }
+}
