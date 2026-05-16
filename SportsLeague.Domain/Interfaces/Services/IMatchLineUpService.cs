f7299ea5cd5530b559c08471cad08edfe0430d4a
Gutz1913 <andresfgutierrezv@gmail.com>
2026-05-16 18:53:13 -0500

    IMatchLineUpService created.

--- SportsLeague.Domain/Interfaces/Services/IMatchLineUpService.cs
+++ SportsLeague.Domain/Interfaces/Services/IMatchLineUpService.cs
@@ -1,5 +1,13 @@
-﻿namespace SportsLeague.Domain.Interfaces.Services;
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Services;
 
 public interface IMatchLineUpService
 {
+    Task<IEnumerable<MatchLineUp>> GetAllAsync();
+    Task<MatchLineUp?> GetByIdAsync(int id);
+    Task<MatchLineUp> CreateAsync(MatchLineUp matchLineUp, int matchId, int playerId, bool isStarter, string position);
+    Task UpdateAsync(int id, MatchLineUp matchLineUp);
+    Task DeleteAsync(int id);
+    Task<IEnumerable<MatchLineUp>> GetByMatchIdAsync(int matchId);
 }
