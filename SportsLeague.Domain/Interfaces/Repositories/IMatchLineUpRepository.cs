71bae71d3da8bf41b0535b959557c250f3f00bcd
Gutz1913 <andresfgutierrezv@gmail.com>
2026-05-16 18:13:33 -0500

    IMatchLineUp Interface created.

---
+++ SportsLeague.Domain/Interfaces/Repositories/IMatchLineUpRepository.cs
@@ -0,0 +1,11 @@
+﻿using SportsLeague.Domain.Entities;
+
+namespace SportsLeague.Domain.Interfaces.Repositories;
+
+public interface IMatchLineUpRepository : IGenericRepository<MatchLineUp>
+{
+    Task<bool> PlayerExistsInLineUpAsync(int matchId, int playerId);
+    Task<int> CountStartersByTeamAndMatchAsync(int matchId, int teamId);
+    Task<IEnumerable<MatchLineUp>> GetByMatchAsync(int matchId);
+    Task<IEnumerable<MatchLineUp>> GetByMatchAndTeamAsync(int matchId, int teamId);
+}
